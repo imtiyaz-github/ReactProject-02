645b89e5d470e85db94fba928848bb057cbea5d9
IMTIYAZ M SHIRASANGI <117428054+imtiyaz-github@users.noreply.github.com>
2023-05-01 11:33:40 +0530

    added ForwardRef

---
+++ Code/06-Finished/src/compenents/MainHeader/Navigation.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import classes from './Navigation.module.css';
+import AuthoContext from '../../store/auth-context';
+
+
+const Navigation = (props) => {
+
+  return (
+    <AuthoContext.Consumer>
+      {(ctx) => {
+        return (
+    <nav className={classes.nav}>
+      <ul>
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Users</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Admin</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <button onClick={props.onLogout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
+}}
+</AuthoContext.Consumer>
+  );
+};
+export default Navigation;
